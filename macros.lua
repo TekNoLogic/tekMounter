95203b0eb6de55da79e6c756a0dfe9221f2db94f
tekkub <tekkub@gmail.com>
2011-02-10 02:36:18 -0700

    All automatic now, thanks to new stuff in 4.0.6

--- macros.lua
+++ macros.lua
@@ -3,10 +3,38 @@ local myname, ns = ...
 
 
 local function macro(combat, ground, flying)
-	ns.isdruid = UnitClass("player") == "Druid"
+end
+
+
+local emergency_spells = {
+	DRUID = "Travel Form(Shapeshift)",
+	PALADIN = "Divine Shield",
+	PRIEST = "Psychic Scream",
+}
+function ns.Scan()
+	local flying, ground, _, class = {}, {}, UnitClass("player")
+	TEKKKKKK = {flying, ground, ns}
+	if UnitRace("player") == "Worgen" then table.insert(ground, "Running Wild(Racial)") end
+	for i=1,GetNumCompanions("MOUNT") do
+		local _, name, spell_id, _, _, mount_type = GetCompanionInfo("MOUNT", i)
+		if bit.band(mount_type, 2) ~= 0 then
+			-- flying mount
+			table.insert(flying, name)
+		elseif bit.band(mount_type, 1) ~= 0 then
+			-- ground mount
+			table.insert(ground, name)
+		end
+	end
+
+	if not next(ground) then return end
+
+	ns.isdruid = class == "DRUID"
+	if ns.isdruid then flying = {GetSpellInfo("Swift Flight Form") or GetSpellInfo("Flight Form")} end
+
+	ns.canfly = not not next(flying)
 
   local t = ""
-  t = t.. "/cast [combat,nomounted] ".. combat.. "\n"
+  t = t.. "/cast [combat,nomounted] ".. emergency_spells[class].. "\n"
   if ns.isdruid then
     t = t.. "/cancelaura [form:1] Bear Form; [form:3] Cat Form; [form:5] Moonkin Form\n"
     t = t.. "/cast [nomounted,swimming] Aquatic Form(Shapeshift)\n"
@@ -15,65 +43,23 @@ local function macro(combat, ground, flying)
 	if crusader then
    	t = t.. "/cast [nomounted] !".. crusader.. "; !Devotion Aura\n"
 	end
- 	if flying then
+ 	if ns.canfly then
     t = t.. "/castrandom [nomounted,nocombat,flyable] ".. table.concat(flying, ", ").. "\n"
   end
-  t = t.. "/castrandom [noswimming,nomounted,nocombat".. (flying and ",noflyable" or "").. "] ".. table.concat(ground, ", ").. "\n"
+  t = t.. "/castrandom [noswimming,nomounted,nocombat".. (ns.canfly and ",noflyable" or "").. "] ".. table.concat(ground, ", ").. "\n"
   t = t.. "/stopmacro [nomounted]\n"
   t = t.. "/dismount [noflying][flying,mod]\n"
   ns.macro = t
 
 	local t = ""
-  t = t.. "#showtooltip [combat,nomounted] ".. combat
+  t = t.. "#showtooltip [combat,nomounted] ".. emergency_spells[class]
   if ns.isdruid then
     t = t.. "; [nomounted,swimming] Aquatic Form(Shapeshift)"
 	  end
- 	if flying then
+ 	if ns.canfly then
     t = t.. "; [flyable] ".. flying[1]
   end
   t = t.. "; ".. ground[1]
 	t = t.. "\n/click tekMounter"
 	ns.placeholder = t
-end
-
-
-local realm, name = GetRealmName(), UnitName("player")
-if realm == "Area 52" then
-
-	if name == "Bear" then
-		macro("Travel Form(Shapeshift)", {
-			"Running Wild(Racial)",
-			"Spotted Frostsaber",
-			"Striped Dawnsaber",
-			"Striped Frostsaber",
-			"Striped Nightsaber",
-		})
-	end
-
-	if name == "Beardyhead" then
-		macro("Psychic Scream", {
-			"Amani War Bear",
-			"Armored Brown Bear",
-			"Big Blizzard Bear",
-		}, {
-			"Red Drake",
-			"Green Proto-Drake",
-		})
-	end
-
-	if name == "Cub" then
-		macro("Divine Shield", {
-			"Big Blizzard Bear",
-			"Swift Stormsaber",
-			"Swift Mistsaber",
-			"Swift Frostsaber",
-			"Armored Brown Bear",
-		}, {
-			"Snowy Gryphon",
-			"Swift Red Gryphon",
-			"Golden Gryphon",
-			"Ebon Gryphon",
-		})
-	end
-
 end
--- tekMounter.lua
+++ tekMounter.lua
@@ -1,34 +1,40 @@
 ﻿
 local myname, ns = ...
-if not ns.macro then return end
+-- if not ns.macro then return end
 
 
 local frame = CreateFrame("Button", "tekMounter", UIParent, "SecureActionButtonTemplate")
 frame:EnableMouse(true)
 frame:RegisterForClicks("AnyUp")
-frame:SetAttribute("type", "macro")
-frame:SetAttribute("macrotext", ns.macro)
 
 frame:RegisterEvent("PLAYER_LOGIN")
+frame:RegisterEvent("COMPANION_UPDATE")
 frame:SetScript("OnEvent", function()
-	if ns.placeholder then
+	ns.Scan()
+	if ns.placeholder and not InCombatLockdown() then
+		frame:SetAttribute("type", "macro")
+		frame:SetAttribute("macrotext", ns.macro)
+
 		local macroid = GetMacroIndexByName("tekMounter")
 		if macroid then EditMacro(macroid, "tekMounter", 1, ns.placeholder, 1) end
-	end
-end)
 
-if ns.isdruid then
-	frame:SetScript("PreClick", function(self)
-		if InCombatLockdown() then return end
+		if ns.isdruid and not ns.druidpreclick then
+			frame:SetScript("PreClick", function(self)
+				if InCombatLockdown() then return end
 
-		if GetUnitSpeed("player") > 0 and not IsSwimming() then
-			frame:SetAttribute("macrotext", [[
-/cast [nomounted] Travel Form(Shapeshift)
+				if GetUnitSpeed("player") > 0 and not IsSwimming() then
+					frame:SetAttribute("macrotext", [[
+/cast [nomounted] ]].. (ns.canfly and "Flight Form(Shapeshift)" or "Travel Form(Shapeshift)").. [[
+
 /stopmacro [nomounted]
 /dismount
 ]])
-		else
-			frame:SetAttribute("macrotext", ns.macro)
+				else
+					frame:SetAttribute("macrotext", ns.macro)
+				end
+			end)
+			ns.druidpreclick = true
 		end
-	end)
-end
+	end
+end)
+
