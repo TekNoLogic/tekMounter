0eee645fc3c69def74a8d9e7cd20cff546e778e7
tekkub ʕ ´ᴥ` ʔ <git@tekkub.net>
2016-11-10 03:51:16 -0700

    dead code

--- macros.lua
+++ macros.lua
@@ -12,27 +12,13 @@ local emergency_spells = {
 	MAGE = "Ice Block",
 	MONK = "Touch of Death",
 }
-local aqmounts = {
-	[25953] = 'blue',
-	[26054] = 'red',
-	[26055] = 'yellow',
-	[26056] = 'green',
-}
-local trademounts = {
-	[44153] = 12656, -- Engineering 300
-	[44151] = 30350, -- Engineering 375
-	[44554] = 12180, -- Tailoring 300
-	[61309] = 51309, -- Tailoring 450
-}
 local multipass = {
 	[121820] = 'flying', -- Obsidian Nightwing
 }
 
 
 function ns.Scan()
-	local ground, multi, multifly = {}, {}, {}
 	local _, class = UnitClass("player")
-
 	ns.isdruid = class == "DRUID"
 
   local t = ""
