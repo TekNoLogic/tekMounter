b0655ade1b5ecdaab338dbecbdf5423ad545a96e
tekkub ʕ ´ᴥ` ʔ <git@tekkub.net>
2016-11-10 18:32:36 -0700

    Move macro out to a constant

--- services/build_macro.lua
+++ services/build_macro.lua
@@ -14,14 +14,19 @@ local emergency_spells = {
 	MONK = "Touch of Death",
 }
 local EMERGENCY_SPELL = emergency_spells[class]
+local MACRO = [[#showtooltip [combat,nomounted] EMERGENCY_SPELL; MOUNT
+/cast [combat,nomounted] EMERGENCY_SPELL
+/stopmacro [combat,nomounted]
+/run C_MountJournal.SummonByID(0)
+]]
+MACRO = MACRO:gsub("EMERGENCY_SPELL", EMERGENCY_SPELL)
+
 
 local multipass = {
-	[121820] = 'flying', -- Obsidian Nightwing
+	[121820] = "flying", -- Obsidian Nightwing
 }
 
 
 function ns.BuildMacro()
-	return "#showtooltip [combat,nomounted] ".. EMERGENCY_SPELL..
-  	"; ".. ns.GetGroundMount().. "\n"..
-		"/run C_MountJournal.SummonByID(0)\n"
+	return MACRO:gsub("MOUNT", ns.GetGroundMount())
 end
