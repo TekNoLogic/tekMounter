c2f5fd81af57899e91cebb8e6595ec8544610653
tekkub ʕ ´ᴥ` ʔ <git@tekkub.net>
2016-11-11 15:20:38 -0700

    Pull multi-mount into own file

--- services/get_ground_mount.lua
+++ services/get_ground_mount.lua
@@ -5,19 +5,8 @@ local myname, ns = ...
 -- http://wow.gamepedia.com/API_C_MountJournal.GetMountInfoExtraByID
 
 
-local _, CLASS = UnitClass("player")
 local GROUND_TYPE = 230
 local IDS = C_MountJournal.GetMountIDs()
-local TRAVEL_FORM = GetSpellInfo(783)
-local MULTI_MOUNTS = {
-	SANDSTONE_DRAKE = GetSpellInfo(93326),
-	X53_ROCKET = GetSpellInfo(75973),
-	OBSIDIAN_NIGHTWING = GetSpellInfo(121820),
-	MEKGINEERS_CHOPPER = GetSpellInfo(60424),
-	MECHANOHOG = GetSpellInfo(55531),
-	EXPEDITION_YAK = GetSpellInfo(122708),
-	TUNDRA_MAMMOTH = GetSpellInfo(61425),
-}
 
 
 local last_mount
@@ -38,16 +27,4 @@ function ns.GetGroundMount()
 	end
 
 	return last_mount
-end
-
-
-function ns.GetMultiMount()
-	if not UnitExists("party1") then return end
-	if UnitExists("party2") then return end
-	if not UnitIsConnected("party1") then return end
-
-	if CLASS == "DRUID" and not IsFlyableArea() then return TRAVEL_FORM end
-	for _,mount in pairs(MULTI_MOUNTS) do
-		if GetSpellInfo(mount) then return mount end
-	end
 end
---
+++ services/get_multi_mount.lua
@@ -0,0 +1,27 @@
+
+local myname, ns = ...
+
+
+local _, CLASS = UnitClass("player")
+local TRAVEL_FORM = GetSpellInfo(783)
+local MULTI_MOUNTS = {
+	SANDSTONE_DRAKE = GetSpellInfo(93326),
+	X53_ROCKET = GetSpellInfo(75973),
+	OBSIDIAN_NIGHTWING = GetSpellInfo(121820),
+	MEKGINEERS_CHOPPER = GetSpellInfo(60424),
+	MECHANOHOG = GetSpellInfo(55531),
+	EXPEDITION_YAK = GetSpellInfo(122708),
+	TUNDRA_MAMMOTH = GetSpellInfo(61425),
+}
+
+
+function ns.GetMultiMount()
+	if not UnitExists("party1") then return end
+	if UnitExists("party2") then return end
+	if not UnitIsConnected("party1") then return end
+
+	if CLASS == "DRUID" and not IsFlyableArea() then return TRAVEL_FORM end
+	for _,mount in pairs(MULTI_MOUNTS) do
+		if GetSpellInfo(mount) then return mount end
+	end
+end
