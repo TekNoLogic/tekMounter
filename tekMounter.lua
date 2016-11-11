0f83fbcaac2beac4848357fdc902babf26934a59
tekkub ʕ ´ᴥ` ʔ <git@tekkub.net>
2016-11-10 18:12:28 -0700

    Pull pre-click stuff into own file

--- tekMounter.lua
+++ tekMounter.lua
@@ -22,37 +22,10 @@ local function UpdateMacro(self)
 		dumpmacro(string.split("\n", ns.macro))
 
 		ns.EditMacro(ns.placeholder)
-
-		if ns.isdruid and not ns.druidpreclick then
-			self:SetScript("PreClick", function(self)
-				if InCombatLockdown() then return end
-
-				if GetUnitSpeed("player") > 0 and not IsSwimming() then
-					local travel = "Travel Form(Shapeshift)"
-					if ns.CanFly() then travel = "Flight Form(Shapeshift)" end
-
-					self:SetAttribute("macrotext", [[
-/cast [nomounted] ]].. (travel).. [[
-
-/stopmacro [nomounted]
-/dismount
-]])
-				else
-					self:SetAttribute("macrotext", ns.macro)
-				end
-			end)
-			ns.druidpreclick = true
-		end
 	end
 end
 
 
-local frame = CreateFrame("Button", "tekMounter", UIParent, "SecureActionButtonTemplate")
-frame:EnableMouse(true)
-frame:RegisterForClicks("AnyUp")
-frame:SetScript("PostClick", UpdateMacro)
-
-
-ns.RegisterCallback(frame, "_COMPANION_UPDATE", UpdateMacro)
-ns.RegisterCallback(frame, "PLAYER_LOGIN", UpdateMacro)
-ns.RegisterCallback(frame, "PLAYER_ENTERING_WORLD", UpdateMacro)
+ns.RegisterCallback("_COMPANION_UPDATE", UpdateMacro)
+ns.RegisterCallback("PLAYER_LOGIN", UpdateMacro)
+ns.RegisterCallback("PLAYER_ENTERING_WORLD", UpdateMacro)
