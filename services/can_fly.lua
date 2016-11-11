2d74a773dd96149b66927932c1021a40d6269aae
tekkub ʕ ´ᴥ` ʔ <git@tekkub.net>
2016-11-10 18:11:55 -0700

    clean up can_fly file a bit

--- services/can_fly.lua
+++ services/can_fly.lua
@@ -2,8 +2,13 @@
 local myname, ns = ...
 
 
+local _, class = UnitClass("player")
+local IS_DRUID = class == "DRUID"
+local FLIGHT = GetSpellInfo(165962)
+local SWIFT_FLIGHT = GetSpellInfo(40123)
+
+
 function ns.CanFly()
-	local _, class = UnitClass("player")
-	if class ~= "DRUID" then return end
-	return not not (GetSpellInfo("Swift Flight Form") or GetSpellInfo("Flight Form"))
+	if not IS_DRUID then return end
+	return not not (GetSpellInfo(SWIFT_FLIGHT) or GetSpellInfo(FLIGHT))
 end
