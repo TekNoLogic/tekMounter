b9dcae3647f89bbcdeadcaaa32d3a7a87e355a8b
tekkub ʕ ´ᴥ` ʔ <git@tekkub.net>
2016-11-11 00:32:47 -0700

    Don't fetch a new table every time

--- services/get_ground_mount.lua
+++ services/get_ground_mount.lua
@@ -8,10 +8,12 @@ local myname, ns = ...
 local GROUND_TYPE = 230
 
 
-function ns.GetGroundMount()
-	local ids = C_MountJournal.GetMountIDs()
+local IDS = C_MountJournal.GetMountIDs()
 
-	for _,id in pairs(ids) do
+
+function ns.GetGroundMount()
+
+	for _,id in pairs(IDS) do
 		local name, _, _, active, usable, _, favorite = C_MountJournal.GetMountInfoByID(id)
 		local _, _, _, is_self_mount, mount_type = C_MountJournal.GetMountInfoExtraByID(id)
 
