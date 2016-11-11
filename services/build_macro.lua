e08423656cb46ceabd6a4bce78f9d7d5c17a8a3b
tekkub ʕ ´ᴥ` ʔ <git@tekkub.net>
2016-11-11 00:32:12 -0700

    Add ghost wolf and travel form

--- services/build_macro.lua
+++ services/build_macro.lua
@@ -15,18 +15,19 @@ local emergency_spells = {
 }
 local EMERGENCY_SPELL = emergency_spells[CLASS]
 local GHOST_WOLF = GetSpellInfo(2645)
+local TRAVEL_FORM = GetSpellInfo(783)
 
 
-local BASE_MACRO = [[#showtooltip [combat,nomounted] EMERGENCY_SPELL; MOUNT
+local SIMPLE_MACRO = [[#showtooltip
+/cast [combat,nomounted] EMERGENCY_SPELL; MOUNT
+]]
+local RANDOM_MACRO = [[#showtooltip [combat,nomounted] EMERGENCY_SPELL; MOUNT
 /cast [combat,nomounted] EMERGENCY_SPELL
 /stopmacro [combat,nomounted]
 /run C_MountJournal.SummonByID(0)
 ]]
-local MOVING_MACRO = [[#showtooltip
-/cast [combat,nomounted] EMERGENCY_SPELL; MOUNT
-]]
-BASE_MACRO = BASE_MACRO:gsub("EMERGENCY_SPELL", EMERGENCY_SPELL)
-MOVING_MACRO = MOVING_MACRO:gsub("EMERGENCY_SPELL", EMERGENCY_SPELL)
+SIMPLE_MACRO = SIMPLE_MACRO:gsub("EMERGENCY_SPELL", EMERGENCY_SPELL)
+RANDOM_MACRO = RANDOM_MACRO:gsub("EMERGENCY_SPELL", EMERGENCY_SPELL)
 
 
 local multipass = {
@@ -35,9 +36,13 @@ local multipass = {
 
 
 function ns.BuildMacro()
-	if CLASS == "SHAMAN" and ns.IsMoving() then
-		return MOVING_MACRO:gsub("MOUNT", GHOST_WOLF)
+	if CLASS == "DRUID" then
+		return SIMPLE_MACRO:gsub("MOUNT", TRAVEL_FORM)
 	end
 
-	return BASE_MACRO:gsub("MOUNT", ns.GetGroundMount())
+	if CLASS == "SHAMAN" and ns.IsMoving() then
+		return SIMPLE_MACRO:gsub("MOUNT", GHOST_WOLF)
+	end
+
+	return RANDOM_MACRO:gsub("MOUNT", ns.GetGroundMount())
 end
