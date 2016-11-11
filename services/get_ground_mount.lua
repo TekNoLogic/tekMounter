98fc4cbf6f9c1760781ff7f08fcd23569a8139aa
tekkub ʕ ´ᴥ` ʔ <git@tekkub.net>
2016-11-11 00:33:07 -0700

    Return active mount, or last used mount

--- services/get_ground_mount.lua
+++ services/get_ground_mount.lua
@@ -11,12 +11,22 @@ local GROUND_TYPE = 230
 local IDS = C_MountJournal.GetMountIDs()
 
 
+local last_mount
 function ns.GetGroundMount()
 
 	for _,id in pairs(IDS) do
 		local name, _, _, active, usable, _, favorite = C_MountJournal.GetMountInfoByID(id)
 		local _, _, _, is_self_mount, mount_type = C_MountJournal.GetMountInfoExtraByID(id)
 
-		if favorite and usable and mount_type == GROUND_TYPE then return name end
+		if active then
+			last_mount = name
+			return name
+		end
+
+		if not last_mount and favorite and usable and mount_type == GROUND_TYPE then
+			last_mount = name
+		end
 	end
+
+	return last_mount
 end
