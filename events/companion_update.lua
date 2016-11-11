43c65f63b7a58c8db9ab8527e7f87b0194468818
tekkub ʕ ´ᴥ` ʔ <git@tekkub.net>
2016-11-10 18:10:59 -0700

    move companion update event file

---
+++ events/companion_update.lua
@@ -0,0 +1,23 @@
+﻿
+local myname, ns = ...
+
+
+local function OnCompanionUpdate(self, message, ...)
+	ns.SendMessage("_COMPANION_UPDATE", ...)
+end
+
+
+local function OnPlayerEnteringWorld(self)
+	ns.RegisterCallback(self, "COMPANION_UPDATE", OnCompanionUpdate)
+end
+
+
+local function OnPlayerLeavingWorld(self)
+	ns.UnregisterCallback(self, "COMPANION_UPDATE")
+end
+
+
+local context = {}
+ns.RegisterCallback(context, "PLAYER_LOGIN", OnPlayerEnteringWorld)
+ns.RegisterCallback(context, "PLAYER_ENTERING_WORLD", OnPlayerEnteringWorld)
+ns.RegisterCallback(context, "PLAYER_LEAVING_WORLD", OnPlayerLeavingWorld)
