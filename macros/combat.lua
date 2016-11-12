195a920dd7c2d000b3714d5de6d5c81ff804bc65
tekkub ʕ ´ᴥ` ʔ <git@tekkub.net>
2016-11-12 16:47:53 -0700

    Make combat macro use `GetClassMount` too

--- macros/combat.lua
+++ macros/combat.lua
@@ -2,9 +2,6 @@
 local myname, ns = ...
 
 
-local _, CLASS = UnitClass("player")
-local GHOST_WOLF = GetSpellInfo(2645)
-local TRAVEL_FORM = GetSpellInfo(783)
 local MACRO = ns.GenerateMacro(
 [[#showtooltip [combat,nomounted] EMERGENCY_SPELL; MOUNT
 /leavevehicle [canexitvehicle]
@@ -16,7 +13,6 @@ local MACRO = ns.GenerateMacro(
 function ns.GetCombatMacro()
 	if not ns.InCombat() then return end
 
-	if CLASS == "DRUID" then return MACRO[TRAVEL_FORM] end
-	if CLASS == "SHAMAN" then return MACRO[GHOST_WOLF] end
-	return MACRO[ns.GetGroundMount()]
+	local mount = ns.GetClassMount() or ns.GetGroundMount()
+	return MACRO[mount]
 end
--- services/get_class_mount.lua
+++ services/get_class_mount.lua
@@ -7,9 +7,14 @@ local GHOST_WOLF = GetSpellInfo(2645)
 local TRAVEL_FORM = GetSpellInfo(783)
 
 
+local function UseGhostWolf()
+	if ns.InCombat() then return true end
+	if IsMounted() then return false end
+	if ns.IsMoving() then return true end
+end
+
+
 function ns.GetClassMount()
 	if CLASS == "DRUID" then return TRAVEL_FORM end
-	if CLASS == "SHAMAN" and not IsMounted() and ns.IsMoving() then
-		return GHOST_WOLF
-	end
+	if CLASS == "SHAMAN" and UseGhostWolf() then return GHOST_WOLF end
 end
